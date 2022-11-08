9eadaa0bb35ae80bf36501e40ea441c3da345f39
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-11-08 12:38:58 +0100

    quest heritage

--- Hangar.java
+++ Hangar.java
@@ -1,3 +1,12 @@
 public class Hangar {
-    
+    public static void main(String[] args) {
+
+        //Car
+        Car clio = new Car("une Clio", 5210);
+        System.out.println(clio.doStuff());
+
+        //Boat
+        Boat catamaran = new Boat("un Catamaran",21008);
+        System.out.println(catamaran.doStuff());
+    }
 }
